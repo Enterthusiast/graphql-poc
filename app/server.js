19536f14b00a4bc3f7c3c4beb5813b6322b4936f
dbernard <dbernard@leadersleague.com>
2017-05-29 18:33:52 +0200

    Added headers and cors options to test the server locally

--- app/server.js
+++ app/server.js
@@ -2,18 +2,25 @@ const graphqlHTTP = require('express-graphql');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const domains = require('./../params/domains');
 const schema = require('./bundles/_root/schema.root');
 
 const app = express();
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "authorization, content-type");
-    next();
-});
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
 }));
+
+app.use("/graphql", function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', domains.cors);
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,
